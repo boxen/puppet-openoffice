303d285edbea06fa0f3e16d0a0cd84d8d3b58ba8
Dieter De Meyer <dieterdemeyer@gmail.com>
2013-02-24 12:32:43 +0100

    Modified source url for package

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class openoffice {
   package { 'OpenOffice':
     provider => 'appdmg',
-    source   => 'http://sourceforge.net/projects/openofficeorg.mirror/files/stable/3.4.1/Apache_OpenOffice_incubating_3.4.1_MacOS_x86_install_en-US.dmg/download',
+    source   => 'http://garr.dl.sourceforge.net/project/openofficeorg.mirror/stable/3.4.1/Apache_OpenOffice_incubating_3.4.1_MacOS_x86_install_en-US.dmg',
   }
 }
